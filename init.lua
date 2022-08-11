ebf48040cc240c68cd5e8a243f0055b8f343965c
Carlton Joseph <carlton.joseph@gmail.com>
2022-08-11 14:20:37 -0700

    add base config

--- init.lua
+++ init.lua
@@ -1,2 +1,1 @@
-print('init.lua loaded')
 require('lua/base')
--- lua/base.lua
+++ lua/base.lua
@@ -1,1 +1,46 @@
-print('base')
+vim.cmd('autocmd!')
+
+vim.scriptencoding = 'utf-8'
+vim.opt.encoding = 'utf-8'
+vim.fileencoding = 'utf-8'
+
+vim.wo.number = true
+
+vim.opt.title = true
+vim.opt.autoindent = true
+vim.opt.hlsearch = true
+vim.opt.backup = false
+vim.opt.showcmd = true
+vim.opt.cmdheight = 1
+vim.opt.laststatus = 2
+vim.opt.expandtab = true
+vim.opt.scrolloff = 10
+vim.opt.shell = 'fish'
+vim.opt.backupskip = '/tmp/*,/private/tmp/*'
+vim.opt.inccommand = 'split'
+vim.opt.ignorecase = true
+vim.opt.smarttab = true
+vim.opt.breakindent = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.ai = true -- auto indent
+vim.opt.si = true -- smart indent
+vim.opt.wrap = true -- no wrap lines
+vim.opt.backspace = 'start,eol,indent'
+vim.opt.path:append { '**' } -- find files into subfolders
+vim.opt.wildignore:append { '*/mode_modules/*' }
+
+-- undercurl
+vim.cmd([[let &t_Cs = "\e[4:3m"]])
+vim.cmd([[let &t_Ce = "\e[4:0m"]])
+-- potentially fails on iTerm2
+
+-- turn off paste mode when leaving insert
+vim.api.nvim_create_autocmd("InsertLeave", {
+  pattern = '*',
+  command = "set nopaste"
+})
+
+-- add astrisks in block comments
+vim.opt.formatoptions:append { 'r' }
+
